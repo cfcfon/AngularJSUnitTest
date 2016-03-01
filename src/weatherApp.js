f437725fb82832697bc707692c5b737b5a50285e
cfcfon <callmefons@gmail.com>
2016-03-02 00:02:20 +0700

    Unit test with AngularJS Controller

--- src/weatherApp.js
+++ src/weatherApp.js
@@ -1,4 +1,5 @@
 var app = angular.module('weatherApp', []);
+
 app.factory('weatherService', function () {
     var service = {};
     service.getWeathers = function () {
@@ -11,4 +12,18 @@ app.factory('weatherService', function () {
         return cities;
     };
     return service;
+});
+
+app.controller('weatherController', function ($scope, weatherService) {
+   $scope.cities = weatherService.getWeathers();
+
+   $scope.cityNames = $scope.cities.map(function (city) {
+       return city.name.toLowerCase();
+   });
+   
+   $scope.search = function (cityName) {
+       var cityIndex = $scope.cityNames.indexOf(cityName.toLowerCase());
+       var city = $scope.cities[cityIndex];
+       return city;
+   };
 });
